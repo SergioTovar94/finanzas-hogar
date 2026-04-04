d023607d75ac5762ea55f42a41e3b0ef58f33083
SergioTovar94 <sergiotovar9430@gmail.com>
2026-04-04 13:18:57 -0500

    Visuales básicos

---
+++ front/src/pages/Settings.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>Configuración de hogar</h1>
+      <p>Preferencias y ajustes de la aplicación</p>
+    </div>
+  );
+}
+
+export default Dashboard;
